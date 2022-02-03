265fff609b93db20e4821360d89f02542c8353dd
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-03 15:40:35 +0300

    feat(dao): add TagDao interface

---
+++ src/main/java/by/zaitsev/dotdottask/model/dao/TagDao.java
@@ -0,0 +1,11 @@
+package by.zaitsev.dotdottask.model.dao;
+
+import by.zaitsev.dotdottask.model.entity.Tag;
+
+/**
+ * TagDao is an interface implemented by classes that will access the tags table.
+ *
+ * @author Kosntantin Zaitsev
+ */
+public interface TagDao extends BaseDao<Tag> {
+}
