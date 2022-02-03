e246d30590df1a7939070f6feb4ad39bdfaa047c
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-03 18:54:10 +0300

    docs(dao): add package-info file

---
+++ src/main/java/by/zaitsev/dotdottask/model/dao/impl/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package contains the main implementations of Dao interfaces.
+ *
+ * @author Konstantin Zaitsev
+ * @see by.zaitsev.dotdottask.model.dao.BaseDao
+ */
+package by.zaitsev.dotdottask.model.dao.impl;
