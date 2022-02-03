10ce97902c851724e72c64156b26f31ae3186c31
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-03 15:39:25 +0300

    feat(dao): add TaskDao interface

---
+++ src/main/java/by/zaitsev/dotdottask/model/dao/TaskDao.java
@@ -0,0 +1,11 @@
+package by.zaitsev.dotdottask.model.dao;
+
+import by.zaitsev.dotdottask.model.entity.Task;
+
+/**
+ * TaskDao is an interface implemented by classes that will access the tasks table.
+ *
+ * @author Kosntantin Zaitsev
+ */
+public interface TaskDao extends BaseDao<Task> {
+}
