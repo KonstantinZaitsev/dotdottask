daacbbc1e0700bb994ef09b89f478bcce4b0abcf
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-03 15:38:16 +0300

    feat(dao): add ProjectDao interface

---
+++ src/main/java/by/zaitsev/dotdottask/model/dao/ProjectDao.java
@@ -0,0 +1,11 @@
+package by.zaitsev.dotdottask.model.dao;
+
+import by.zaitsev.dotdottask.model.entity.Project;
+
+/**
+ * ProjectDao is an interface implemented by classes that will access the projects table.
+ *
+ * @author Kosntantin Zaitsev
+ */
+public interface ProjectDao extends BaseDao<Project> {
+}
