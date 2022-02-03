2b12a387d65829c575a3c5d7765c3b4d72111202
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-03 19:48:14 +0300

    docs(service): add package-info file

---
+++ src/main/java/by/zaitsev/dotdottask/model/service/impl/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package contains the main implementations of Service interfaces.
+ *
+ * @author Konstantin Zaitsev
+ * @see by.zaitsev.dotdottask.model.service.BaseService
+ */
+package by.zaitsev.dotdottask.model.service.impl;
