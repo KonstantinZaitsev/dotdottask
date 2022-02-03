4386a6f0b9d1cb1562eb2e6b5decfab78824b5e2
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-03 15:07:26 +0300

    feat(entity): add Task class

---
+++ src/main/java/by/zaitsev/dotdottask/model/entity/Task.java
@@ -0,0 +1,123 @@
+package by.zaitsev.dotdottask.model.entity;
+
+import java.sql.Timestamp;
+import java.util.StringJoiner;
+
+/**
+ * Task is the entity class used in the TaskDao.
+ *
+ * @author Konstantin Zaitsev
+ */
+public class Task extends AbstractEntity {
+    private long projectId;
+    private String title;
+    private String description;
+    private Timestamp creationDate;
+    private Timestamp deadline;
+    private boolean isDone;
+    private long assignedUserId;
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Timestamp getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Timestamp deadline) {
+        this.deadline = deadline;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public long getAssignedUserId() {
+        return assignedUserId;
+    }
+
+    public void setAssignedUserId(long assignedUserId) {
+        this.assignedUserId = assignedUserId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        Task that = (Task) o;
+        return projectId == that.projectId &&
+                isDone == that.isDone &&
+                assignedUserId == that.assignedUserId &&
+                title != null ? title.equals(that.title) : that.title == null &&
+                description != null ? description.equals(that.description) : that.description == null &&
+                creationDate != null ? creationDate.equals(that.creationDate) : that.creationDate == null &&
+                deadline != null ? deadline.equals(that.deadline) : that.deadline == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (int) (projectId ^ (projectId >>> 32));
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (creationDate != null ? creationDate.hashCode() : 0);
+        result = 31 * result + (deadline != null ? deadline.hashCode() : 0);
+        result = 31 * result + (isDone ? 1 : 0);
+        result = 31 * result + (int) (assignedUserId ^ (assignedUserId >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Task.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("projectId=" + projectId)
+                .add("title='" + title + "'")
+                .add("description='" + description + "'")
+                .add("creationDate=" + creationDate)
+                .add("deadline=" + deadline)
+                .add("isDone=" + isDone)
+                .add("assignedUserId=" + assignedUserId)
+                .toString();
+    }
+}
