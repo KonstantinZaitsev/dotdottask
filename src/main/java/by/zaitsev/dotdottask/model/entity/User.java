558669fdd6b78636895f98db9f1e7e43c80fa43e
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-03 15:17:54 +0300

    feat(entity): add boolean isAuthorized field to User class

--- src/main/java/by/zaitsev/dotdottask/model/entity/User.java
+++ src/main/java/by/zaitsev/dotdottask/model/entity/User.java
@@ -12,6 +12,7 @@ public class User extends AbstractEntity {
     private String name;
     private String surname;
     private String image;
+    private boolean isAuthorized = false;
 
     public String getEmail() {
         return email;
@@ -45,13 +46,22 @@ public class User extends AbstractEntity {
         this.image = image;
     }
 
+    public boolean isAuthorized() {
+        return isAuthorized;
+    }
+
+    public void setAuthorized(boolean authorized) {
+        isAuthorized = authorized;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         User that = (User) o;
-        return email != null ? email.equals(that.email) : that.email == null &&
+        return isAuthorized == that.isAuthorized &&
+                email != null ? email.equals(that.email) : that.email == null &&
                 name != null ? name.equals(that.name) : that.name == null &&
                 surname != null ? surname.equals(that.surname) : that.surname == null &&
                 image != null ? image.equals(that.image) : that.image == null;
@@ -75,6 +85,7 @@ public class User extends AbstractEntity {
                 .add("name='" + name + "'")
                 .add("surname='" + surname + "'")
                 .add("image='" + image + "'")
+                .add("isAuthorized=" + isAuthorized)
                 .toString();
     }
 }
