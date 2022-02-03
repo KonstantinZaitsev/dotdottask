5d808c198bd467ca9714906e552efa4e9a3c04fb
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-03 17:00:09 +0300

    feat(util): add ParameterIndex class

---
+++ src/main/java/by/zaitsev/dotdottask/util/ParameterIndex.java
@@ -0,0 +1,23 @@
+package by.zaitsev.dotdottask.util;
+
+import java.sql.PreparedStatement;
+
+/**
+ * ParameterIndex is a class with integer constants used for {@link PreparedStatement}.
+ *
+ * @author Konstantin Zaitsev
+ */
+public final class ParameterIndex {
+    public static final int FIRST = 1;
+    public static final int SECOND = 2;
+    public static final int THIRD = 3;
+    public static final int FOURTH = 4;
+    public static final int FIFTH = 5;
+    public static final int SIXTH = 6;
+    public static final int SEVENTH = 7;
+    public static final int EIGHTH = 8;
+    public static final int NINTH = 9;
+
+    private ParameterIndex() {
+    }
+}
