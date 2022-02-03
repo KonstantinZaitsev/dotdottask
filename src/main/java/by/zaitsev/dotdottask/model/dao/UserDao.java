d1586489494cab77d202ef4be0cf29dd20dbd3ba
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-03 15:37:09 +0300

    feat(dao): add UserDao interface

---
+++ src/main/java/by/zaitsev/dotdottask/model/dao/UserDao.java
@@ -0,0 +1,11 @@
+package by.zaitsev.dotdottask.model.dao;
+
+import by.zaitsev.dotdottask.model.entity.User;
+
+/**
+ * UserDao is an interface implemented by classes that will access the users table.
+ *
+ * @author Kosntantin Zaitsev
+ */
+public interface UserDao extends BaseDao<User> {
+}
