b73b08376761df10c4c53ee7772b4ce0165519d2
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-04 10:32:08 +0300

    feat(util): add PagePath class

---
+++ src/main/java/by/zaitsev/dotdottask/util/PagePath.java
@@ -0,0 +1,16 @@
+package by.zaitsev.dotdottask.util;
+
+import by.zaitsev.dotdottask.controller.Router;
+
+/**
+ * PagePath is a class with paths to jsp pages of the application.
+ *
+ * @author Konstantin Zaitsev
+ * @see Router
+ */
+public final class PagePath {
+    public static final String INDEX_PAGE = "/index.jsp";
+
+    private PagePath() {
+    }
+}
