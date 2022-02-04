4eb6499f90fcdda082e20e8517cc83eaa9fd9d26
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-04 11:03:53 +0300

    feat(util): add AttributeName class

---
+++ src/main/java/by/zaitsev/dotdottask/util/AttributeName.java
@@ -0,0 +1,13 @@
+package by.zaitsev.dotdottask.util;
+
+/**
+ * AttributeName is a class with attribute names used in jsp pages.
+ *
+ * @author Konstantin Zaitsev
+ */
+public final class AttributeName {
+    public static final String WRONG_COMMAND = "wrong_command";
+
+    private AttributeName() {
+    }
+}
