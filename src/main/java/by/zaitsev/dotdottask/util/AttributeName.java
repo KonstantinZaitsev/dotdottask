f6a8c7fad77648bdfc2eaa69eca68ca36c87f493
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-04 13:54:00 +0300

    feat(util): add new attribute name

--- src/main/java/by/zaitsev/dotdottask/util/AttributeName.java
+++ src/main/java/by/zaitsev/dotdottask/util/AttributeName.java
@@ -9,6 +9,7 @@ public final class AttributeName {
     public static final String WRONG_COMMAND = "wrong_command";
     public static final String USER = "user";
     public static final String CURRENT_PAGE = "current_page";
+    public static final String LOCALE = "locale";
 
     private AttributeName() {
     }
