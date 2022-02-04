525b1ed1a40eaf538cd19c916826c9faaab8ab0e
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-04 21:33:21 +0300

    feat(service): add insertNewUser method to UserService interface and UserServiceImpl class

--- src/main/java/by/zaitsev/dotdottask/model/service/UserService.java
+++ src/main/java/by/zaitsev/dotdottask/model/service/UserService.java
@@ -1,7 +1,10 @@
 package by.zaitsev.dotdottask.model.service;
 
+import by.zaitsev.dotdottask.exception.ServiceException;
 import by.zaitsev.dotdottask.model.entity.User;
 import by.zaitsev.dotdottask.model.dao.UserDao;
+
+import java.util.Map;
 
 /**
  * UserService is an interface implemented by a class that will access classes that implement the
@@ -10,4 +13,10 @@ import by.zaitsev.dotdottask.model.dao.UserDao;
  * @author Konstantin Zaitsev
  */
 public interface UserService extends BaseService<User> {
+    /**
+     * @param parameterMap map with parameters passed from request.
+     * @return entity id if it was added, otherwise 0.
+     * @throws ServiceException if the request to Dao class could not be handled.
+     */
+    long insertNewUser(Map<String, String> parameterMap) throws ServiceException;
 }
--- src/main/java/by/zaitsev/dotdottask/model/service/impl/UserServiceImpl.java
+++ src/main/java/by/zaitsev/dotdottask/model/service/impl/UserServiceImpl.java
@@ -6,11 +6,16 @@ import by.zaitsev.dotdottask.model.dao.UserDao;
 import by.zaitsev.dotdottask.model.dao.impl.UserDaoImpl;
 import by.zaitsev.dotdottask.model.entity.User;
 import by.zaitsev.dotdottask.model.service.UserService;
+import by.zaitsev.dotdottask.util.ParameterName;
+import by.zaitsev.dotdottask.util.PasswordEncryptor;
+import by.zaitsev.dotdottask.util.validator.UserValidator;
+import by.zaitsev.dotdottask.util.validator.ValidationResult;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -82,5 +87,64 @@ public class UserServiceImpl implements UserService {
             throw new ServiceException("Unable to delete user from database. Dao access error: ", e);
         }
         return isDeleted;
+    }
+
+    @Override
+    public long insertNewUser(Map<String, String> parameterMap) throws ServiceException {
+        String email = parameterMap.get(ParameterName.EMAIL);
+        String password = parameterMap.get(ParameterName.PASSWORD);
+        String confirmedPassword = parameterMap.get(ParameterName.CONFIRMED_PASSWORD);
+        String name = parameterMap.get(ParameterName.NAME);
+        String surname = parameterMap.get(ParameterName.SURNAME);
+        var userValidator = UserValidator.getInstance();
+        long id = 0;
+        try {
+            boolean isEmailExist = userDao.findUserByEmail(email).isPresent();
+            String emailCheckResult;
+            if (userValidator.isEmailValid(email)) {
+                emailCheckResult = !isEmailExist ? ValidationResult.VALID : ValidationResult.NOT_UNIQUE;
+            } else {
+                emailCheckResult = ValidationResult.INVALID;
+            }
+            String passwordCheckResult = userValidator.isPasswordValid(password) ?
+                    ValidationResult.VALID : ValidationResult.INVALID;
+            String confirmedPasswordCheckResult = password.equals(confirmedPassword) ?
+                    ValidationResult.VALID : ValidationResult.INVALID;
+            String nameCheckResult = userValidator.isNameValid(name) ?
+                    ValidationResult.VALID : ValidationResult.INVALID;
+            String surnameCheckResult = userValidator.isSurnameValid(surname) ?
+                    ValidationResult.VALID : ValidationResult.INVALID;
+            boolean isEmailValid = Boolean.parseBoolean(emailCheckResult);
+            boolean isPasswordValid = Boolean.parseBoolean(passwordCheckResult);
+            boolean isConfirmedPasswordValid = Boolean.parseBoolean(confirmedPasswordCheckResult);
+            boolean isNameValid = Boolean.parseBoolean(nameCheckResult);
+            boolean isSurnameValid = Boolean.parseBoolean(surnameCheckResult);
+            boolean result = isEmailValid &&
+                    isPasswordValid &&
+                    isConfirmedPasswordValid &&
+                    isNameValid &&
+                    isSurnameValid;
+            if (result) {
+                var user = new User();
+                user.setEmail(email);
+                user.setName(name);
+                user.setSurname(surname);
+                String encryptedPassword = PasswordEncryptor.getInstance().encrypt(password);
+                id = userDao.insertNewUser(user, encryptedPassword);
+            } else {
+                parameterMap.replace(ParameterName.EMAIL, emailCheckResult);
+                parameterMap.replace(ParameterName.PASSWORD, passwordCheckResult);
+                parameterMap.replace(ParameterName.CONFIRMED_PASSWORD, confirmedPasswordCheckResult);
+                parameterMap.replace(ParameterName.NAME, nameCheckResult);
+                parameterMap.replace(ParameterName.SURNAME, surnameCheckResult);
+            }
+            logger.log(Level.DEBUG, "insertNewUser(Map<String, String> parameterMap) method was completed " +
+                    "successfully. User " + (id != 0 ? "with id {} was added" : "wasn't added"), id);
+        } catch (DaoException e) {
+            logger.log(Level.ERROR, "Unable to insert new user. Dao access error: {}",
+                    e.getMessage());
+            throw new ServiceException("Unable to insert new user. Dao access error: ", e);
+        }
+        return id;
     }
 }
