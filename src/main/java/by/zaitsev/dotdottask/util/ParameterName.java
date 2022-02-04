bebf45bdf2bb9502d72cec6387d13f43f87e3eec
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-04 21:31:19 +0300

    feat(util): add new parameter names

--- src/main/java/by/zaitsev/dotdottask/util/ParameterName.java
+++ src/main/java/by/zaitsev/dotdottask/util/ParameterName.java
@@ -8,6 +8,11 @@ package by.zaitsev.dotdottask.util;
 public final class ParameterName {
     public static final String COMMAND = "command";
     public static final String LOCALE = "locale";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String CONFIRMED_PASSWORD = "confirmed_password";
+    public static final String NAME = "name";
+    public static final String SURNAME = "surname";
 
     private ParameterName() {
     }
