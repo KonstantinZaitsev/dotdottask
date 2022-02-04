ffe731697f8eb2f843399d0e78820306a37550c2
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-04 11:46:26 +0300

    feat(util): add ParameterName class

---
+++ src/main/java/by/zaitsev/dotdottask/util/ParameterName.java
@@ -0,0 +1,13 @@
+package by.zaitsev.dotdottask.util;
+
+/**
+ * ParameterName contains the parameter names from request.
+ *
+ * @author Konstantin Zaitsev
+ */
+public final class ParameterName {
+    public static final String COMMAND = "command";
+
+    private ParameterName() {
+    }
+}
