e64a0f3f714d8c5ac0618d78853dd08337a8470b
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-05 21:05:39 +0300

    fix(util): fix catalog page path

--- src/main/java/by/zaitsev/dotdottask/util/PagePath.java
+++ src/main/java/by/zaitsev/dotdottask/util/PagePath.java
@@ -13,7 +13,7 @@ public final class PagePath {
     public static final String ERROR_400_PAGE = "/jsp/error/error400.jsp";
     public static final String SIGN_IN_PAGE = "/jsp/guest/sign-in.jsp";
     public static final String SIGN_UP_PAGE = "/jsp/guest/sign-up.jsp";
-    public static final String CATALOG_PAGE = "/jsp/user/catalog/jsp";
+    public static final String CATALOG_PAGE = "/jsp/user/catalog.jsp";
 
     private PagePath() {
     }
