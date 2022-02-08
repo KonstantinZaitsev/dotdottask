e969ba6dc7992f442310264d69826cc8bd31da32
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-08 18:05:47 +0300

    fix(controller): add to CurrentPageFilter forward dispatcher type

--- src/main/java/by/zaitsev/dotdottask/controller/filter/CurrentPageFilter.java
+++ src/main/java/by/zaitsev/dotdottask/controller/filter/CurrentPageFilter.java
@@ -20,7 +20,7 @@ import java.io.IOException;
  * @author Konstantin Zaitsev
  */
 @WebFilter(urlPatterns = "*.jsp",
-        dispatcherTypes = {DispatcherType.REQUEST},
+        dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD},
         initParams = {@WebInitParam(name = "PAGES_ROOT_DIRECTORY", value = "/jsp"),
                 @WebInitParam(name = "INDEX_PAGE", value = "/index.jsp")})
 public class CurrentPageFilter implements Filter {
