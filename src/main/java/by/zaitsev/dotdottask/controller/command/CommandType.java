0d970e1c708e5e2fc795e287b2cd3f4408b94e4e
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-08 10:01:29 +0300

    feat(controller): add sign out command to command types

--- src/main/java/by/zaitsev/dotdottask/controller/command/CommandType.java
+++ src/main/java/by/zaitsev/dotdottask/controller/command/CommandType.java
@@ -8,6 +8,7 @@ import by.zaitsev.dotdottask.controller.command.impl.navigation.GoToCatalogPageP
 import by.zaitsev.dotdottask.controller.command.impl.navigation.GoToIndexPageCommand;
 import by.zaitsev.dotdottask.controller.command.impl.navigation.GoToSignInPageCommand;
 import by.zaitsev.dotdottask.controller.command.impl.navigation.GoToSignUpPageCommand;
+import by.zaitsev.dotdottask.controller.command.impl.user.SignOutCommand;
 import by.zaitsev.dotdottask.model.entity.User;
 
 import java.util.EnumSet;
@@ -26,7 +27,8 @@ public enum CommandType {
     GO_TO_CATALOG_PAGE_COMMAND(new GoToCatalogPagePathCommand(), EnumSet.of(User.UserRole.GUEST)),
     GO_TO_SIGN_IN_PAGE_COMMAND(new GoToSignInPageCommand(), EnumSet.of(User.UserRole.GUEST)),
     GO_TO_SIGN_UP_PAGE_COMMAND(new GoToSignUpPageCommand(), EnumSet.of(User.UserRole.GUEST)),
-    GO_TO_INDEX_PAGE_COMMAND(new GoToIndexPageCommand(), EnumSet.of(User.UserRole.GUEST));
+    GO_TO_INDEX_PAGE_COMMAND(new GoToIndexPageCommand(), EnumSet.of(User.UserRole.GUEST)),
+    SIGN_OUT_COMMAND(new SignOutCommand(), EnumSet.of(User.UserRole.USER));
 
     private final Command command;
     private final EnumSet<User.UserRole> allowedRoles;
