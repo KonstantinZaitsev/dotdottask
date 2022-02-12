7c01384f2f705fea0d01b6e28e3ff54eba6984eb
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-12 18:54:35 +0300

    feat(service): add update methods for TaskService interface and TaskServiceImpl class

--- src/main/java/by/zaitsev/dotdottask/model/service/TaskService.java
+++ src/main/java/by/zaitsev/dotdottask/model/service/TaskService.java
@@ -4,6 +4,7 @@ import by.zaitsev.dotdottask.exception.ServiceException;
 import by.zaitsev.dotdottask.model.dao.TaskDao;
 import by.zaitsev.dotdottask.model.entity.Task;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -19,4 +20,44 @@ public interface TaskService extends BaseService<Task> {
      * @throws ServiceException if the request to Dao class could not be handled.
      */
     List<Task> findAllTasksByProjectId(long id) throws ServiceException;
+
+    /**
+     * @param id    task id to update.
+     * @param title new task title.
+     * @return true if the user has been updated, otherwise false.
+     * @throws ServiceException if the request to Dao class could not be handled.
+     */
+    boolean updateTaskTitleById(long id, String title) throws ServiceException;
+
+    /**
+     * @param id          task id to update.
+     * @param description new task description.
+     * @return true if the user has been updated, otherwise false.
+     * @throws ServiceException if the request to Dao class could not be handled.
+     */
+    boolean updateTaskDescriptionById(long id, String description) throws ServiceException;
+
+    /**
+     * @param id       task id to update.
+     * @param deadline new task deadline.
+     * @return true if the user has been updated, otherwise false.
+     * @throws ServiceException if the request to Dao class could not be handled.
+     */
+    boolean updateTaskDeadlineById(long id, Timestamp deadline) throws ServiceException;
+
+    /**
+     * @param id     task id to update.
+     * @param isDone new task is done status.
+     * @return true if the user has been updated, otherwise false.
+     * @throws ServiceException if the request to Dao class could not be handled.
+     */
+    boolean updateTaskIsDoneById(long id, boolean isDone) throws ServiceException;
+
+    /**
+     * @param id             task id to update.
+     * @param assignedUserId new task assigned user id.
+     * @return true if the user has been updated, otherwise false.
+     * @throws ServiceException if the request to Dao class could not be handled.
+     */
+    boolean updateTaskAssignedUserIdById(long id, long assignedUserId) throws ServiceException;
 }
--- src/main/java/by/zaitsev/dotdottask/model/service/impl/TaskServiceImpl.java
+++ src/main/java/by/zaitsev/dotdottask/model/service/impl/TaskServiceImpl.java
@@ -12,6 +12,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -121,5 +122,80 @@ public class TaskServiceImpl implements TaskService {
             throw new ServiceException("Unable to find tasks by project id. Dao access error: ", e);
         }
         return taskList;
+    }
+
+    @Override
+    public boolean updateTaskTitleById(long id, String title) throws ServiceException {
+        boolean isUpdated;
+        try {
+            isUpdated = taskDao.updateTaskTitleById(id, title);
+            logger.log(Level.DEBUG, "updateTaskTitleById(long id, String title) method was completed " +
+                    "successfully. Task with id {} " + (isUpdated ? "was updated" : "wasn't updated"), id);
+        } catch (DaoException e) {
+            logger.log(Level.ERROR, "Unable to update task title by id. Dao access error: {}",
+                    e.getMessage());
+            throw new ServiceException("Unable to update task title by id. Dao access error: ", e);
+        }
+        return isUpdated;
+    }
+
+    @Override
+    public boolean updateTaskDescriptionById(long id, String description) throws ServiceException {
+        boolean isUpdated;
+        try {
+            isUpdated = taskDao.updateTaskDescriptionById(id, description);
+            logger.log(Level.DEBUG, "updateTaskDescriptionById(long id, String description) method was " +
+                    "completed successfully. Task with id {} " + (isUpdated ? "was updated" : "wasn't updated"), id);
+        } catch (DaoException e) {
+            logger.log(Level.ERROR, "Unable to update task description by id. Dao access error: {}",
+                    e.getMessage());
+            throw new ServiceException("Unable to update task description by id. Dao access error: ", e);
+        }
+        return isUpdated;
+    }
+
+    @Override
+    public boolean updateTaskDeadlineById(long id, Timestamp deadline) throws ServiceException {
+        boolean isUpdated;
+        try {
+            isUpdated = taskDao.updateTaskDeadlineById(id, deadline);
+            logger.log(Level.DEBUG, "updateTaskDeadlineById(long id, Timestamp deadline) method was " +
+                    "completed successfully. Task with id {} " + (isUpdated ? "was updated" : "wasn't updated"), id);
+        } catch (DaoException e) {
+            logger.log(Level.ERROR, "Unable to update task deadline by id. Dao access error: {}",
+                    e.getMessage());
+            throw new ServiceException("Unable to update task deadline by id. Dao access error: ", e);
+        }
+        return isUpdated;
+    }
+
+    @Override
+    public boolean updateTaskIsDoneById(long id, boolean isDone) throws ServiceException {
+        boolean isUpdated;
+        try {
+            isUpdated = taskDao.updateTaskIsDoneById(id, isDone);
+            logger.log(Level.DEBUG, "updateTaskIsDoneById(long id, boolean isDone) method was completed " +
+                    "successfully. Task with id {} " + (isUpdated ? "was updated" : "wasn't updated"), id);
+        } catch (DaoException e) {
+            logger.log(Level.ERROR, "Unable to update task is done status by id. Dao access error: {}",
+                    e.getMessage());
+            throw new ServiceException("Unable to update task is done status by id. Dao access error: ", e);
+        }
+        return isUpdated;
+    }
+
+    @Override
+    public boolean updateTaskAssignedUserIdById(long id, long assignedUserId) throws ServiceException {
+        boolean isUpdated;
+        try {
+            isUpdated = taskDao.updateTaskAssignedUserIdById(id, assignedUserId);
+            logger.log(Level.DEBUG, "updateTaskAssignedUserIdById(long id, long assignedUserId) method was " +
+                    "completed successfully. Task with id {} " + (isUpdated ? "was updated" : "wasn't updated"), id);
+        } catch (DaoException e) {
+            logger.log(Level.ERROR, "Unable to update task assigned user id by id. Dao access error: {}",
+                    e.getMessage());
+            throw new ServiceException("Unable to update task assigned user id by id. Dao access error: ", e);
+        }
+        return isUpdated;
     }
 }
