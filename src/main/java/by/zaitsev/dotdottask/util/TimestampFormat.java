05b444a075b36e3a2f002d621186a9505b8834e9
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-13 12:43:40 +0300

    feat(util): add TimestampFormat class

---
+++ src/main/java/by/zaitsev/dotdottask/util/TimestampFormat.java
@@ -0,0 +1,9 @@
+package by.zaitsev.dotdottask.util;
+
+public final class TimestampFormat {
+    public static final String CREATION_DATE_FORMAT = "yyyy-mm-dd hh:mm";
+    public static final String DEADLINE_FORMAT = "yyy-mm-dd hh:mm";
+
+    private TimestampFormat() {
+    }
+}
