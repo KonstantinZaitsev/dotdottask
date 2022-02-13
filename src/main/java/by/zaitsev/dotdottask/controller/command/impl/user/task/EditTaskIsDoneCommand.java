d957729c1c84b8e59d3e75cabcd8fd6b76d70dcc
Konstantin Zaitsev <sea.of.fish@protonmail.com>
2022-02-13 13:35:41 +0300

    feat(controller): add EditTaskIsDoneCommand class

---
+++ src/main/java/by/zaitsev/dotdottask/controller/command/impl/user/task/EditTaskIsDoneCommand.java
@@ -0,0 +1,76 @@
+package by.zaitsev.dotdottask.controller.command.impl.user.task;
+
+import by.zaitsev.dotdottask.controller.Router;
+import by.zaitsev.dotdottask.controller.command.Command;
+import by.zaitsev.dotdottask.exception.CommandException;
+import by.zaitsev.dotdottask.exception.ServiceException;
+import by.zaitsev.dotdottask.model.entity.Project;
+import by.zaitsev.dotdottask.model.entity.Task;
+import by.zaitsev.dotdottask.model.service.impl.TaskServiceImpl;
+import by.zaitsev.dotdottask.util.AttributeName;
+import by.zaitsev.dotdottask.util.PagePath;
+import by.zaitsev.dotdottask.util.ParameterName;
+import by.zaitsev.dotdottask.util.validator.TaskValidator;
+import by.zaitsev.dotdottask.util.validator.ValidationResult;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.Optional;
+
+public class EditTaskIsDoneCommand implements Command {
+    private static final Logger logger = LogManager.getLogger(EditTaskIsDoneCommand.class);
+
+    @Override
+    @SuppressWarnings(value = "unchecked")
+    public Router execute(HttpServletRequest request) throws CommandException {
+        HttpSession session = request.getSession();
+        var ownProjects = (List<Project>) session.getAttribute(AttributeName.OWN_PROJECTS);
+        String isDone = request.getParameter(ParameterName.IS_DONE);
+        var projectId = Long.parseLong(request.getParameter(ParameterName.PROJECT_ID));
+        var taskId = Long.parseLong(request.getParameter(ParameterName.TASK_ID));
+        Optional<Project> optionalProject = ownProjects
+                .stream()
+                .filter(value -> value.getId() == projectId)
+                .findFirst();
+        Optional<Task> optionalTask;
+        if (optionalProject.isPresent()) {
+            optionalTask = optionalProject.get().getTaskList()
+                    .stream()
+                    .filter(value -> value.getId() == taskId)
+                    .findFirst();
+        } else {
+            logger.log(Level.ERROR, "Cannot update task is done status, there are no project with id {}",
+                    projectId);
+            throw new CommandException("Cannot update task is done status, there are no project with id " + projectId);
+        }
+        Task task;
+        if (optionalTask.isPresent()) {
+            task = optionalTask.get();
+        } else {
+            logger.log(Level.ERROR, "Cannot update task is done status, there are no task with id {}", taskId);
+            throw new CommandException("Cannot update task is done status, there are no task with id " + taskId);
+        }
+        var taskValidator = TaskValidator.getInstance();
+        try {
+            if (taskValidator.isIsDoneValid(isDone)) {
+                boolean newIsDone = Boolean.parseBoolean(isDone);
+                TaskServiceImpl.getInstance().updateTaskIsDoneById(taskId, newIsDone);
+                task.setDone(newIsDone);
+                logger.log(Level.DEBUG, "execute(HttpServletRequest request) method was completed " +
+                        "successfully. Forwarded to catalog page");
+            } else {
+                request.setAttribute(AttributeName.UPDATE_TASK_IS_DONE_RESULT, ValidationResult.INVALID);
+            }
+        } catch (ServiceException e) {
+            logger.log(Level.ERROR, "Task cannot be updated: {}", e.getMessage());
+            throw new CommandException("Task cannot be updated: ", e);
+        }
+        var router = new Router();
+        router.setPagePath(PagePath.CATALOG_PAGE);
+        return router;
+    }
+}
